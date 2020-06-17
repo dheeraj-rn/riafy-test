d38d11dea1f68e71c151f79df80d51b49189c40a
Dheeraj <dheer047@gmail.com>
2020-06-17 17:36:29 +0530

    medium thumbnail for mobile device

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const app = express();
 const axios = require("axios");
+const device = require('express-device');
 
 const PORT = process.env.PORT || 3000;
 
+
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+app.use(device.capture());
 
 app.get("/", async (req, res) => {
     let instaData = await axios.get('https://hiit.ria.rocks/videos_api/cdn/com.rstream.crafts?versionCode=40&lurl=Canvas%20painting%20ideas');
     res.render("index", {
-        instaData: instaData.data
+        instaData: instaData.data,
+        device: req.device.type.toUpperCase()
     });
 });
 
