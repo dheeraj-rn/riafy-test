1741e57ba04189b8a4587a973ff111c47ecd7075
Dheeraj <dheer047@gmail.com>
2020-06-17 22:45:33 +0530

    removed express-device

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 const express = require("express");
 const app = express();
 const axios = require("axios");
-const device = require('express-device');
 
 const PORT = process.env.PORT || 3000;
 
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-app.use(device.capture());
 
 app.get("/", async (req, res) => {
     let instaData = await axios.get('https://hiit.ria.rocks/videos_api/cdn/com.rstream.crafts?versionCode=40&lurl=Canvas%20painting%20ideas');
     let comments = await axios.get('http://cookbookrecipes.in/test.php');
     res.render("index", {
         instaData: instaData.data,
-        device: req.device.type.toUpperCase(),
         comments: comments.data
     });
 });
